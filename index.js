37c9e2900a89065bea995050971582a41232773e
joker00777 <vansh7uppal@gmail.com>
2021-10-22 20:03:44 +0530

    pushing to git

--- index.js
+++ index.js
@@ -25,10 +25,9 @@ mongoose.connect(dbUrl, {
         console.log(err);
     })
 
-
+app.set('view engine', 'ejs');
 app.engine('ejs',ejsMate);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');    
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
